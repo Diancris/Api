2df579fa4b4c36699adbbe5a2cc4e8bb551c4674
Diancris <diana.romecoy@gmail.com>
2023-02-02 10:48:09 +0100

    Cambios

---
+++ Entities/Entities/DeliveredStatusItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class DeliveredStatusItem
+    {
+        public DeliveredStatusItem() { }
+        public int Id { get; set; }
+        public string CreatedOrder { get; set; }
+        public string InProcess { get; set; }
+        public string Status { get; set; }
+        public string Transit { get; set; }
+        public string Delivered { get; set; }
+        public DateTime CreatedDate { get; set;}
+    }
+}
